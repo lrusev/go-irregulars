4dc3742b6fb0cc81fd400a82165a00095a604611
lrusev <rusev.leonid@gmail.com>
2015-10-30 16:29:32 +0200

    added cli interface

--- db.go
+++ db.go
@@ -7,8 +7,13 @@ import (
 func getVerbs(count int) ([]Verb, error) {
     var rows *sql.Rows
     var err error
-
-    rows, err = db.Query("SELECT * FROM verbs ORDER BY RANDOM() LIMIT $1", count)
+    
+    if driver == "mysql" {
+            rows, err = db.Query("SELECT * FROM verbs ORDER BY RAND() LIMIT ?", count)
+        } else {
+            rows, err = db.Query("SELECT * FROM verbs ORDER BY RANDOM() LIMIT $1", count)
+            
+        }
     if err != nil {
         return nil, err
     }
@@ -41,7 +46,11 @@ func getTotalVerbs() (int) {
 }
 
 func insert(inf, simple, participle, trans string) (sql.Result, error) {
-    return db.Exec("INSERT INTO verbs VALUES (default, $1, $2, $3, $4)", inf, simple, participle, trans)
+    if driver == "mysql" {
+        return db.Exec("INSERT INTO verbs VALUES (null, ?, ?, ?, ?)", inf, simple, participle, trans)
+    } else {
+        return db.Exec("INSERT INTO verbs VALUES (default, $1, $2, $3, $4)", inf, simple, participle, trans)
+    }
 }
 
 
--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
     "fmt"
     "log"
+    "os"
     "os/user"
     "database/sql"
     "github.com/FogCreek/mini"
     _ "github.com/lib/pq"
+    _ "github.com/go-sql-driver/mysql"
     "encoding/json"
     "io/ioutil"
+    "gopkg.in/readline.v1"
 )
+
+const myconf string = ".myverbs"
+const pqconf string = ".verbs"
 
 type Verb struct {
     Id              int    `json:"id"`
@@ -31,39 +37,70 @@ func chk(err error) {
     }
 }
 
-func params() string {
+func connect() (*sql.DB, error) {
+    var info string
+
     u, err := user.Current()
     fatal(err)
-    cfg, err := mini.LoadConfiguration(".verbs")
-    fatal(err)
 
-    info := fmt.Sprintf("host=%s port=%s dbname=%s "+
-        "sslmode=%s user=%s password=%s ",
-        cfg.String("host", "127.0.0.1"),
-        cfg.String("port", "5432"),
-        cfg.String("dbname", u.Username),
-        cfg.String("sslmode", "disable"),
-        cfg.String("user", u.Username),
-        cfg.String("pass", ""),
-    )
-    return info
+    if _, err = os.Stat(myconf); os.IsNotExist(err) {
+        cfg, err := mini.LoadConfiguration(pqconf)
+        fatal(err)
+
+        info = fmt.Sprintf("host=%s port=%s dbname=%s "+
+            "sslmode=%s user=%s password=%s ",
+            cfg.String("host", "127.0.0.1"),
+            cfg.String("port", "5432"),
+            cfg.String("dbname", u.Username),
+            cfg.String("sslmode", "disable"),
+            cfg.String("user", u.Username),
+            cfg.String("pass", ""),
+        )
+
+        driver = "postgres"
+    } else {
+        cfg, err := mini.LoadConfiguration(myconf)
+        fatal(err)
+
+        driver = "mysql"
+        info = fmt.Sprintf("%s:%s@/%s?charset=utf8mb4,utf8&collation=utf8_general_ci",
+            cfg.String("user", u.Username),
+            cfg.String("pass", ""),
+            cfg.String("dbname", u.Username),
+        )
+    }
+
+    return sql.Open(driver, info)
 }
 
 var db *sql.DB
+var driver string
 
 func main () {
     var err error
-    db, err = sql.Open("postgres", params())
+
+    db, err = connect()
     fatal(err)
     defer db.Close()
 
-    _, err = db.Exec("CREATE TABLE IF NOT EXISTS " +
-        `verbs("id" SERIAL PRIMARY KEY,` +
-        `"infinitive" varchar(100) not null,` +
-        `"past_simpe" varchar(100) not null,` +
-        `"past_participle" varchar(100) not null,` +
-        `"translation" varchar(100) not null` +
-        `)`)
+    if driver == "mysql" {
+        _, err = db.Exec("CREATE TABLE IF NOT EXISTS " +
+            `verbs(id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,` +
+            `infinitive varchar(100) not null,` +
+            `past_simpe varchar(100) not null,` +
+            `past_participle varchar(100) not null,` +
+            `translation varchar(100) not null` +
+            `) engine=InnoDb Default charset=utf8 collate=utf8_general_ci`)
+    } else {
+        _, err = db.Exec("CREATE TABLE IF NOT EXISTS " +
+            `verbs("id" SERIAL PRIMARY KEY,` +
+            `"infinitive" varchar(100) not null,` +
+            `"past_simpe" varchar(100) not null,` +
+            `"past_participle" varchar(100) not null,` +
+            `"translation" varchar(100) not null` +
+            `)`)
+    }
+
     fatal(err)
     total:= getTotalVerbs();
     file, err := ioutil.ReadFile("./fixtures/irregulars.json")
@@ -77,20 +114,53 @@ func main () {
 
     if len(recs) > total {
         for i := 0; i < len(recs); i++ {
-            fmt.Printf("Add new verb: %s\n", recs[i].Infinitive)
+            fmt.Printf("Add new verb: %s %v\n", recs[i].Infinitive, recs[i])
             _, err = insert(recs[i].Infinitive, recs[i].Past_simpe, recs[i].Past_participle, recs[i].Translation)
             chk(err)
 
         }
     }
 
+    
+    count := 10
 
 
-    verbs, err := getVerbs(10)
+    verbs, err := getVerbs(count)
     chk(err)
+
+    fmt.Printf("Start with %d words...\n", count)
+
+    /*for v := 0; v < len(verbs); v++ {
+        fmt.Printf("%d) %s\n", v+1, verbs[v].Translation)
+    }*/
+
+    rl, err := readline.New("> ")
+    if err != nil {
+        panic(err)
+    }
+
+    defer rl.Close()
+
+    var valid string
+    correct, incorrect := 0, 0
 
     for v := 0; v < len(verbs); v++ {
         fmt.Printf("%d) %s\n", v+1, verbs[v].Translation)
+        line, err := rl.Readline()
+        if err != nil { // io.EOF
+            break
+        }
+
+        valid = fmt.Sprintf("%s %s %s", verbs[v].Infinitive, verbs[v].Past_simpe, verbs[v].Past_participle)
+        if line == valid {
+            correct +=1
+            println("valid")
+        } else {
+            incorrect+=1
+            println("invalid(", valid, ")")
+        }
     }
 
+    println(fmt.Sprintf("Correct: %d", correct))
+    println(fmt.Sprintf("In-correct: %d", incorrect))
 }
